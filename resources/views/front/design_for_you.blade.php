fc5e75f3dcf1fb34fec390fd901a5c8c28b215c2
Shashwat Mishra <shashwatmishra2403@gmail.com>
2025-05-27 15:21:21 +0530

    call us at design you apply image

--- resources/views/front/design_for_you.blade.php
+++ resources/views/front/design_for_you.blade.php
@@ -18,7 +18,10 @@
 <div class="container-req">
     <p>At Ready to print , we understand the importance of presenting print-ready files for a smooth printing process. Most of our clients provide us with these files, and we offer a complimentary check to ensure everything is in order before proceeding to print. If we identify any issues, we will promptly inform you to avoid any complications during production. Additionally, should you require assistance in designing from scratch, we have  skilled designers available to assist you, whether you need a basic design or a more complex project. For a separate design quote, please click on the link below, and one of our team members will be happy to provide you with the information you need. And have something like this
     </p>
-    <img src="{{ asset('front-assets/images/design_for_you.png') }}" alt="Design for you" class="img-fluid" />
+    <a href="tel:0296213111">
+        <img src="{{ asset('front-assets/images/design_for_you.png') }}" alt="Design for you" class="img-fluid" />
+    </a>
+    <!-- <img src="{{ asset('front-assets/images/design_for_you.png') }}" alt="Design for you" class="img-fluid" /> -->
 @endsection
 
 @section('javascript.js')
